4c23792e73aa33fc856834042758c94e51d68d8d
mtochenov <tochenovm@gmail.com>
2021-10-24 14:00:33 +0300

    separate weather func as weather-app from main-app

--- main/urls.py
+++ main/urls.py
@@ -2,8 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='home'),
+    path('', views.home, name='home'),
     path('about/', views.about, name='about'),
     path('contacts/', views.contacts, name='contacts'),
-    path('weather/', views.weather, name='weather')
 ]
--- main/views.py
+++ main/views.py
@@ -1,14 +1,13 @@
 from django.shortcuts import render
-import requests
 
 
-def index(request):
+def home(request):
     """ Отображение шаблона главной страницы """
     data = {
         'title': 'Главная страница',
     }
 
-    return render(request, 'main/index.html', data)
+    return render(request, 'main/home.html', data)
 
 
 def about(request):
@@ -20,49 +19,3 @@ def about(request):
 
 def contacts(request):
     return render(request, 'main/contacts.html')
-
-
-cities_dict = {
-    "Moscow,RU": "Москва",
-    "Velikiy Novgorod,RU": "Великий Новгород",
-    "Nizhniy Novgorod,RU": "Нижний Новгород",
-    "Saint Petersburg,RU": "Санкт-Петерсбург",
-    "Kyiv,UA": "Киев",
-    "Minsk,BY": "Минск",
-    "Tivat,ME": "Тиват, Черногория",
-}
-
-
-def weather(request):
-    """
-    Получает данные о погоде с сейта 'openweathermap.org' Данные по бесплатному тарифу.
-    Данные обрабатываются в формате json.
-    """
-    if request.method == "POST":
-        city = request.POST['location']
-
-    else:
-        city = "Moscow,RU"
-
-    appid = "8bfe1216baff61f145a193def0c5b772"
-    url = "https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=" + appid
-
-    city_name = cities_dict[city]
-
-    res = requests.get(url.format(city)).json()
-
-    city_info = {
-        'city': city,
-        'city_name': city_name,
-        'temp': round(res['main']['temp'], 1),
-        'pressure': round(((res['main']['pressure']) * 0.750063), 1),  # Переводим в мм ртутного столба
-        'wind': res['wind']['speed'],
-        'icon': res['weather'][0]['icon'],
-    }
-
-    data = {
-        'info': city_info,
-        'title': 'Погода',
-    }
-
-    return render(request, "main/weather.html", data)
