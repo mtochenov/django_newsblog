39765e556210c7e13bb370741320b6098c66f8f2
mtochenov <tochenovm@gmail.com>
2022-03-30 08:35:08 +0300

    add url tests

--- news/tests.py
+++ news/tests.py
@@ -1,3 +1,27 @@
 from django.test import TestCase
+from django.urls import reverse, resolve
 
-# Create your tests here.
+from .views import *
+
+
+class TestUrls(TestCase):
+
+    def test_news_list_resolved(self):
+        url = reverse('news_list')
+        self.assertEquals(resolve(url).func.view_class, NewsListView)
+
+    def test_news_detail_resolved(self):
+        url = reverse('news_detail', kwargs={'pk': 1})
+        self.assertEquals(resolve(url).func.view_class, NewsDetailView)
+
+    def test_news_add_resolved(self):
+        url = reverse('create')
+        self.assertEquals(resolve(url).func, news_create)
+
+    def test_news_update_resolved(self):
+        url = reverse('news_update', kwargs={'pk': 1})
+        self.assertEquals(resolve(url).func.view_class, NewsUpdateView)
+
+    def test_news_delete_resolved(self):
+        url = reverse('news_delete', kwargs={'pk': 1})
+        self.assertEquals(resolve(url).func.view_class, NewsDeleteView)
